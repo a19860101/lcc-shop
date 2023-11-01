969b5b1d9b1fd2a61943f4c9c8bd8bbdb6e5df96
Zac <a19860101@gmail.com>
2023-11-01 20:59:54 +0800

    20231101

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,47 @@
-@extends('template.master')
-@section('main')
-<h1>hello</h1>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+    <nav class="bg-amber-200 py-10">
+        <div class="container mx-auto flex flex-wrap">
+            <div class="w-1/3"></div>
+            <div class="w-1/3 text-center">
+                <a href="#" class="text-2xl font-weight text-red-500">
+                LOGO
+                </a>
+            </div>
+            <div class="w-1/3 text-right">
+                <a href="#">登入</a>
+                <a href="#">註冊</a>
+            </div>
+            <div class="w-full text-center mt-5">
+                <a href="#">品牌故事</a>
+                <a href="#">所有商品</a>
+                <a href="#">期間限定</a>
+                <a href="#">優惠活動</a>
+            </div>
+        </div>
+    </nav>
+    <header class="w-full aspect-[21/9]">
+        <img src="https://picsum.photos/id/35/1600/900" alt="" class="w-full h-full object-cover">
+    </header>
+    <section class="bg-blue-600 h-[600px] relative w-full">
+        <div class="w-full h-[300px] absolute top-[-100px] left-[-200px]">
+            <img src="https://picsum.photos/id/33/1600/900" alt=""class="w-full h-full object-cover">
+        </div>
+        <div>
+            <h2>title</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora et quam, fugit nisi totam debitis obcaecati blanditiis architecto minima labore expedita dolorum. Asperiores doloremque officiis eaque quo, nemo iste eum.
+            </p>
+        </div>
+    </section>
+</body>
+</html>
